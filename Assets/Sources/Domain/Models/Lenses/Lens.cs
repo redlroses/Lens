3cd617b7222f2a3a1298059437f0154d40de2ce5
Evgeniy Batenkov <redlroses.rlr@gmail.com>
2024-01-19 17:02:34 +0500

    Added Lens model

---
+++ Assets/Sources/Domain/Models/Lenses/Lens.cs
@@ -0,0 +1,12 @@
+﻿using Game.Domain.Lenses.Surfaces;
+
+namespace Game.Domain.Models.Lenses
+{
+    public class Lens
+    {
+        private Surface _frontSurface;
+        private Surface _backSurface;
+        private float _thickness;
+        private float _radius;
+    }
+}
